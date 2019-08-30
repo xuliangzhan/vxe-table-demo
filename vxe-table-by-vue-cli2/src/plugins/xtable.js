d9a9d2385835f311ecba7651ae2a5bfb2f94fc9b
xuliangzhan <xu_liangzhan@163.com>
2019-08-30 12:51:59 +0800

    update

--- vxe-table-by-vue-cli2/src/plugins/xtable.js
+++ vxe-table-by-vue-cli2/src/plugins/xtable.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
-import VXETable from 'vxe-table/packages/v-x-e-table'
-import Table from 'vxe-table/packages/table'
-import Header from 'vxe-table/packages/header'
-import Body from 'vxe-table/packages/body'
-import Column from 'vxe-table/packages/column'
+import {
+  VXETable,
+  Icon,
+  Table,
+  Header,
+  Body,
+  Column
+} from 'vxe-table'
 import zhCNLocat from 'vxe-table/lib/locale/lang/zh-CN'
-import 'vxe-table/lib/index.css'
 
 // 按需导入需要的模块
+Vue.use(Icon)
 Vue.use(Table)
-Vue.use(Column)
 Vue.use(Header)
 Vue.use(Body)
+Vue.use(Column)
 
 // 导入默认的国际化（如果项目中使用多语言，则应该导入到 vue-i18n 中）
 VXETable.setup({
