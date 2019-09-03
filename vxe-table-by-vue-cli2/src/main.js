f0e085786d8323f20fb9b2ec7b35d3f241cb6673
xuliangzhan <xu_liangzhan@163.com>
2019-09-03 09:44:48 +0800

    update

--- vxe-table-by-vue-cli2/src/main.js
+++ vxe-table-by-vue-cli2/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import './plugins/utils'
 import './plugins/xtable'
 
 Vue.config.productionTip = false
--- vxe-table-by-vue-cli2/src/plugins/xtable.js
+++ vxe-table-by-vue-cli2/src/plugins/xtable.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import 'xe-utils'
 import VXETable from 'vxe-table'
 import 'vxe-table/lib/index.css'
 
