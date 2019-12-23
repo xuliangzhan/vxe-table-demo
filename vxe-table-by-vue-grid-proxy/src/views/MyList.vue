c71caad2c16a91911bca10b4002605b576161980
xuliangzhan <xu_liangzhan@163.com>
2019-12-23 18:32:06 +0800

    update

--- vxe-table-by-vue-grid-proxy/src/views/MyList.vue
+++ vxe-table-by-vue-grid-proxy/src/views/MyList.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <vxe-grid
-      resizable
-      :filter-config="{remote: true}"
-      :columns="tableColumn"
-      :proxy-config="tableProxy"
-      :checkbox-config="{labelField: 'id', reserve: true, highlight: true, range: true}">
-    </vxe-grid>
+    <vxe-grid v-bind="xGridOptions"></vxe-grid>
   </div>
 </template>
 
@@ -14,34 +8,45 @@
 export default {
   data () {
     return {
-      tableProxy: {
-        sort: true,
-        filter: true,
-        ajax: {
-          query: '/api/user/list'
-        }
-      },
-      tableColumn: [
-        { type: 'seq', width: 60, fixed: 'left' },
-        { type: 'checkbox', title: 'ID', width: 140, fixed: 'left' },
-        { field: 'name', title: 'Name', remoteSort: true },
-        { field: 'nickname', title: 'Nickname', remoteSort: true },
-        { field: 'age', title: 'Age', remoteSort: true },
-        {
-          field: 'role',
-          title: 'Role',
-          remoteSort: true,
-          width: 200,
-          filters: [
-            { label: '前端开发', value: '前端' },
-            { label: '后端开发', value: '后端' },
-            { label: '测试', value: '测试' },
-            { label: '程序员鼓励师', value: '程序员鼓励师' }
-          ],
-          filterMultiple: false
+      xGridOptions: {
+        resizable: true,
+        filterConfig: {
+          remote: true
         },
-        { field: 'describe', title: 'Describe', showOverflow: true }
-      ]
+        checkboxConfig: {
+          labelField: 'id',
+          highlight: true,
+          range: true
+        },
+        proxyConfig: {
+          sort: true,
+          filter: true,
+          ajax: {
+            query: '/api/user/list'
+          }
+        },
+        columns: [
+          { type: 'seq', width: 60, fixed: 'left' },
+          { type: 'checkbox', title: 'ID', width: 140, fixed: 'left' },
+          { field: 'name', title: 'Name', remoteSort: true },
+          { field: 'nickname', title: 'Nickname', remoteSort: true },
+          { field: 'age', title: 'Age', remoteSort: true },
+          {
+            field: 'role',
+            title: 'Role',
+            remoteSort: true,
+            width: 200,
+            filters: [
+              { label: '前端开发', value: '前端' },
+              { label: '后端开发', value: '后端' },
+              { label: '测试', value: '测试' },
+              { label: '程序员鼓励师', value: '程序员鼓励师' }
+            ],
+            filterMultiple: false
+          },
+          { field: 'describe', title: 'Describe', showOverflow: true }
+        ]
+      }
     }
   }
 }
--- vxe-table-by-vue-grid-proxy/src/views/MyPage.vue
+++ vxe-table-by-vue-grid-proxy/src/views/MyPage.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <vxe-grid
-      resizable
-      height="528"
-      :filter-config="{remote: true}"
-      :pager-config="tablePage"
-      :columns="tableColumn"
-      :proxy-config="tableProxy"
-      :checkbox-config="{labelField: 'id', reserve: true, highlight: true, range: true}">
-    </vxe-grid>
+    <vxe-grid v-bind="xGridOptions"></vxe-grid>
   </div>
 </template>
 
@@ -16,39 +8,51 @@
 export default {
   data () {
     return {
-      tablePage: {
-        pageSize: 15,
-        pageSizes: [5, 10, 20, 50, 100, 200, 500, 1000]
-      },
-      tableProxy: {
-        index: true,
-        sort: true,
-        filter: true,
-        ajax: {
-          query: '/api/user/page/list/{{page.pageSize}}/{{page.currentPage}}'
-        }
-      },
-      tableColumn: [
-        { type: 'seq', width: 60, fixed: 'left' },
-        { type: 'checkbox', title: 'ID', width: 140, fixed: 'left' },
-        { field: 'name', title: 'Name', remoteSort: true },
-        { field: 'nickname', title: 'Nickname', remoteSort: true },
-        { field: 'age', title: 'Age', remoteSort: true },
-        {
-          field: 'role',
-          title: 'Role',
-          remoteSort: true,
-          width: 200,
-          filters: [
-            { label: '前端开发', value: '前端' },
-            { label: '后端开发', value: '后端' },
-            { label: '测试', value: '测试' },
-            { label: '程序员鼓励师', value: '程序员鼓励师' }
-          ],
-          filterMultiple: false
+      xGridOptions: {
+        resizable: true,
+        height: 528,
+        filterConfig: {
+          remote: true
         },
-        { field: 'describe', title: 'Describe', showOverflow: true }
-      ]
+        checkboxConfig: {
+          labelField: 'id',
+          highlight: true,
+          range: true
+        },
+        pagerConfig: {
+          pageSize: 15,
+          pageSizes: [5, 10, 20, 50, 100, 200, 500, 1000]
+        },
+        proxyConfig: {
+          index: true,
+          sort: true,
+          filter: true,
+          ajax: {
+            query: '/api/user/page/list/{{page.pageSize}}/{{page.currentPage}}'
+          }
+        },
+        columns: [
+          { type: 'seq', width: 60, fixed: 'left' },
+          { type: 'checkbox', title: 'ID', width: 140, fixed: 'left' },
+          { field: 'name', title: 'Name', remoteSort: true },
+          { field: 'nickname', title: 'Nickname', remoteSort: true },
+          { field: 'age', title: 'Age', remoteSort: true },
+          {
+            field: 'role',
+            title: 'Role',
+            remoteSort: true,
+            width: 200,
+            filters: [
+              { label: '前端开发', value: '前端' },
+              { label: '后端开发', value: '后端' },
+              { label: '测试', value: '测试' },
+              { label: '程序员鼓励师', value: '程序员鼓励师' }
+            ],
+            filterMultiple: false
+          },
+          { field: 'describe', title: 'Describe', showOverflow: true }
+        ]
+      }
     }
   }
 }
