735300a6449b45f9662e7d84d4b8c07b0d157d5f
xuliangzhan <xu_liangzhan@163.com>
2020-01-02 16:03:04 +0800

    update

--- vxe-table-by-vue-grid-proxy/src/plugins/xtable.js
+++ vxe-table-by-vue-grid-proxy/src/plugins/xtable.js
@@ -9,33 +9,45 @@ Vue.use(VXETable)
 // 设置默认参数
 VXETable.setup({
   grid: {
-    // 封装统一的代理配置，任何支持 Promise 的异步请求库都能对接，不同的库可能对应参数会不一样（fetch、jquery、axios、xe-ajax）
+    // 封装统一的代理配置，任何支持 Promise 的异步请求库都能对接，不同的库可能用法会不一样，基本大同小异（fetch、jquery、axios、xe-ajax）
     proxyConfig: {
       // 查询
-      beforeQuery ({ options, page, sort, filters }) {
+      beforeQuery (params) {
+        const { options, page, sort, filters } = params
+        if (XEUtils.isFunction(options)) {
+          return options(params)
+        }
         // 处理排序条件
-        let formData = {
+        const queryParams = {
           sort: sort.property,
           order: sort.order
         }
         // 处理筛选条件
         filters.forEach(({ property, values }) => {
-          formData[property] = values.join(',')
+          queryParams[property] = values.join(',')
         })
-        let ajaxOpts = Object.assign({ method: 'GET', params: formData }, XEUtils.isString(options) ? { url: options } : options)
+        const ajaxOpts = Object.assign({ method: 'GET', params: queryParams }, XEUtils.isString(options) ? { url: options } : options)
         if (page) {
           ajaxOpts.url = XEUtils.template(ajaxOpts.url, { page })
         }
         return XEAjax(ajaxOpts).then(response => response.json())
       },
       // 删除
-      beforeDelete ({ options, body }) {
-        let ajaxOpts = Object.assign({ method: 'POST', body }, XEUtils.isString(options) ? { url: options } : options)
+      beforeDelete (params) {
+        const { options, body } = params
+        if (XEUtils.isFunction(options)) {
+          return options(params)
+        }
+        const ajaxOpts = Object.assign({ method: 'POST', body }, XEUtils.isString(options) ? { url: options } : options)
         return XEAjax(ajaxOpts).then(response => response.json())
       },
       // 保存
-      beforeSave ({ options, body }) {
-        let ajaxOpts = Object.assign({ method: 'POST', body }, XEUtils.isString(options) ? { url: options } : options)
+      beforeSave (params) {
+        const { options, body } = params
+        if (XEUtils.isFunction(options)) {
+          return options(params)
+        }
+        const ajaxOpts = Object.assign({ method: 'POST', body }, XEUtils.isString(options) ? { url: options } : options)
         return XEAjax(ajaxOpts).then(response => response.json())
       }
     }
