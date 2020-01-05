2e6cdf391b22da8c488ce1720d894a83ef1a6891
xuliangzhan <xu_liangzhan@163.com>
2020-01-05 15:25:29 +0800

    update

--- vxe-table-by-vue-grid-proxy/src/plugins/ajax.js
+++ vxe-table-by-vue-grid-proxy/src/plugins/ajax.js
@@ -1,5 +1,7 @@
-import Vue from 'vue'
-import XEAjax from 'xe-ajax'
+import 'xe-ajax'
 
-// 挂载到 vue 实例中
-Vue.prototype.$ajax = XEAjax
+// import Vue from 'vue'
+// import XEAjax from 'xe-ajax'
+
+// // 挂载到 vue 实例中
+// Vue.prototype.$ajax = XEAjax
