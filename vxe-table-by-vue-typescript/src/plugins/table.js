6de009e6835eb9ddea44b4d88c7ab1c5cd7ffbd0
xuliangzhan <xu_liangzhan@163.com>
2020-03-27 23:49:21 +0800

    update

--- vxe-table-by-vue-typescript/src/plugins/table.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import VXETable from 'vxe-table'
-import 'vxe-table/lib/index.css'
-
-Vue.use(VXETable)
--- vxe-table-by-vue-typescript/src/plugins/table.ts
+++ vxe-table-by-vue-typescript/src/plugins/table.ts
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import VXETable, { ModalController } from 'vxe-table'
-import '../assets/table/style.scss'
+import 'vxe-table/lib/index.css'
 
 Vue.use(VXETable)
 
 // 将模态窗口挂载到 vue 全局实例中
 Vue.prototype.$XModal = ModalController
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $XModal: typeof ModalController;
+  }
+}
