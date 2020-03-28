63755e0d028d32e96f825d840a6708d1fad30933
xuliangzhan <xu_liangzhan@163.com>
2020-03-28 18:12:16 +0800

    update

--- vxe-table-by-vue-typescript/src/plugins/table.ts
+++ vxe-table-by-vue-typescript/src/plugins/table.ts
@@ -4,7 +4,7 @@ import 'vxe-table/lib/index.css'
 
 Vue.use(VXETable)
 
-// 将模态窗口挂载到 vue 全局实例中
+// 将模态窗口挂载到 vue 实例中
 Vue.prototype.$XModal = ModalController
 
 declare module 'vue/types/vue' {
--- vxe-table-by-vue-typescript/src/plugins/utils.ts
+++ vxe-table-by-vue-typescript/src/plugins/utils.ts
@@ -3,5 +3,11 @@ import 'xe-utils'
 // import Vue from 'vue'
 // import XEUtils from 'xe-utils'
 
-// // 挂载到 vue 实例中
+// // 将工具类挂载到 vue 实例中
 // Vue.prototype.$utils = XEUtils
+
+// declare module 'vue/types/vue' {
+//   interface Vue {
+//     $utils: typeof XEUtils;
+//   }
+// }
