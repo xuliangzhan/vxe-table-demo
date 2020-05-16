d6efc0af6559ed719628fc767b7a592ed1d04433
xuliangzhan <xu_liangzhan@163.com>
2020-05-16 17:28:29 +0800

    update

--- vxe-table-by-vue-custom-theme/src/components/MyTable.vue
+++ vxe-table-by-vue-custom-theme/src/components/MyTable.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
+    <vxe-toolbar>
+      <template v-slot:buttons>
+        <vxe-button status="primary">按钮1</vxe-button>
+      </template>
+    </vxe-toolbar>
+
     <vxe-table
       border
+      round
       highlight-hover-row
       :data="tableData">
       <vxe-table-column type="seq" title="Number" width="80"></vxe-table-column>
@@ -9,6 +16,12 @@
       <vxe-table-column field="sex" title="Sex" sortable></vxe-table-column>
       <vxe-table-column field="address" title="Address"></vxe-table-column>
     </vxe-table>
+
+    <vxe-pager
+      :current-page.sync="tablePage.currentPage"
+      :page-size.sync="tablePage.pageSize"
+      :total="tablePage.totalResult">
+    </vxe-pager>
   </div>
 </template>
 
@@ -17,6 +30,11 @@ export default {
   name: 'MyTable',
   data () {
     return {
+      tablePage: {
+        currentPage: 1,
+        pageSize: 15,
+        totalResult: 0
+      },
       tableData: [
         {
           id: 10001,
