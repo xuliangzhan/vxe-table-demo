dc8f0cd3705ab06b37b7817941878176fe6659d4
xuliangzhan <xu_liangzhan@163.com>
2025-02-17 17:22:48 +0800

    update docs

--- vxe-table-by-vue2-demand/src/main.js
+++ vxe-table-by-vue2-demand/src/main.js
@@ -3,7 +3,43 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import './plugins'
+import {
+  VxeUI,
+  VxeIcon,
+  VxeButton,
+  VxeInput,
+  VxeLoading,
+  VxeTooltip
+} from 'vxe-pc-ui'
+
+import {
+  VxeTable,
+  VxeColumn,
+  VxeColgroup,
+  VxeGrid
+} from 'vxe-table'
+
+// 导入主题变量，也可以重写主题变量
+import 'vxe-table/styles/cssvar.scss'
+import 'vxe-pc-ui/styles/cssvar.scss'
+
+// 导入默认的语言
+import zhCN from 'vxe-pc-ui/lib/language/zh-CN'
+
+VxeUI.setI18n('zh-CN', zhCN)
+VxeUI.setLanguage('zh-CN')
+
+// 注册组件，由于没有全局安装，所以使用时需要逐个 import
+Vue.use(VxeIcon)
+Vue.use(VxeButton)
+Vue.use(VxeInput)
+Vue.use(VxeLoading)
+Vue.use(VxeTooltip)
+
+Vue.use(VxeTable)
+Vue.use(VxeColumn)
+Vue.use(VxeColgroup)
+Vue.use(VxeGrid)
 
 Vue.config.productionTip = false
 
--- vxe-table-by-vue3-theme/src/main.js
+++ vxe-table-by-vue3-theme/src/main.js
@@ -6,7 +6,12 @@ import store from './store'
 import VxeUITable from 'vxe-table'
 import 'vxe-table/lib/style.css'
 
-import VxeUI from 'vxe-pc-ui'
-import 'vxe-pc-ui/lib/style.css'
+// import VxeUI from 'vxe-pc-ui'
+// import 'vxe-pc-ui/lib/style.css'
 
-createApp(App).use(store).use(router).use(VxeUI).use(VxeUITable).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  // .use(VxeUI)
+  .use(VxeUITable)
+  .mount('#app')
